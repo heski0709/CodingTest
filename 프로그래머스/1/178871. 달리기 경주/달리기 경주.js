b43eefbb6e4822f963066da803c936721d5e1868
heski0709 <131249274+heski0709@users.noreply.github.com>
2024-01-28 16:44:31 +0900

    [level 1] Title: 달리기 경주, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,17 +1,15 @@
 function solution(players, callings) {
-    const obj = {}
-    players.forEach((player, index) => {
-        obj[player] = index + 1
-    })
+    const obj = {};
+    players.forEach((player, index) => (obj[player] = index));
 
     callings.forEach((call) => {
-        const prevPlayer = players[obj[call] - 2]
-        const prevIndex = obj[prevPlayer]++ - 1
-        const callIndex = obj[call]-- - 1
+        const prevPlayer = players[obj[call] - 1];
+        const prevIndex = obj[prevPlayer]++;
+        const callIndex = obj[call]--;
 
-        players[prevIndex] = players[callIndex]
-        players[callIndex] = prevPlayer
-    })
+        players[prevIndex] = players[callIndex];
+        players[callIndex] = prevPlayer;
+    });
 
     return players;
 }
